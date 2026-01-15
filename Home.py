ae609b5ae7515b17348254acccd4bf8ad4b55872
DollarStoreThor <arado0702@gmail.com>
2026-01-15 01:15:48 -0600

    working with plotly charting

--- Home.py
+++ Home.py
@@ -6,16 +6,18 @@ import streamlit as st
 df = pd.read_excel('Workout 2026.xlsx')[:6]
 DOW = df.columns
 
-Excercises_Per_Day = {}
+Workouts_Per_Day = {}
 for day in DOW:
     current_day_excecises = df[day]
-    Excercises_Per_Day[day] = []
+    Workouts_Per_Day[day] = []
 
 
     #print(current_day_excecises.dropna())
 
     for item in current_day_excecises.dropna()[1:]:
         if item != "Relax - Stretch - Yoga":
-            Excercises_Per_Day[day].append(pd.read_excel('Workout 2026.xlsx', sheet_name=item)['Excercise Name'][0])
+            Workouts_Per_Day[day].append(pd.read_excel('Workout 2026.xlsx', sheet_name=item)['Excercise Name'][0])
+    
+
 
 
